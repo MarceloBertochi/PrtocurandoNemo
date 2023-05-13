76ec8fed0bada34c7593b25b6bab7b02c9405876
MarceloBertochi <paidasamia@gmail.com>
2023-05-13 16:01:29 -0300

    Exercício Valdir

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Clear();
+String Texto, Baleeis;
+
+Console.WriteLine("Digite um texto.");
+Texto = Console.ReadLine();
+
+Console.WriteLine();
+
+Baleeis = Texto
+.Replace("a", "aaa")
+.Replace("e", "eee")
+.Replace("i", "iii")
+.Replace("o", "ooo")
+.Replace("u", "uuu")
+.Replace("A", "AAA")
+.Replace("E", "EEE")
+.Replace("I", "III")
+.Replace("O", "OOO")
+.Replace("U", "UUU");
+Console.WriteLine(Baleeis);
+
+
